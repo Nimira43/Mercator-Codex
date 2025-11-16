18716d96c1a0259b3bf0bc2db41bebadbad5b5f6
Nimira43 <n.rabone72@aol.com>
2025-11-16 19:26:15 +0000

    Created three ne button variants and a transitioning class for links and buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
+import { Button } from "@/components/ui/button";
+
 export default function HomePage() {
   return (
-    <div>
+    <>
+      <div>
       <h1 className='text-3xl font-medium p-6 bg-main-dark text-main-light border-b-4 border-main logo'>
         Mercator Codex
       </h1>
     </div>
+    <div className='mt-10 p-6'>
+      <Button variant='mainBtn' className='m-2 transitioning'>Login</Button>
+      <Button variant='mainDarkBtn' className='m-2 transitioning'>Register</Button>
+      <Button variant='mainLightBtn' className='m-2 transitioning'>Submit</Button>
+    </div>
+    </>
+    
   )
 }
