0787e9c64e74136d4b5aeeaf80ddd9b7e451eb41
Nimira43 <n.rabone72@aol.com>
2025-11-16 18:35:58 +0000

    Tailwind 3 set up

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 export default function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1 className='text-3xl font-medium p-6 bg-gray-900 text-white'>
+        Mercator Codex
+      </h1>
+    </div>
   )
 }
