4b11460afd665c79d2d661caa6e8d02643e72be0
Nimira43 <n.rabone72@aol.com>
2025-11-16 19:03:09 +0000

    Brought in and set up logo font.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div>
-      <h1 className='text-3xl font-medium p-6 bg-main-dark text-main-light border-b-4 border-main'>
+      <h1 className='text-3xl font-medium p-6 bg-main-dark text-main-light border-b-4 border-main logo'>
         Mercator Codex
       </h1>
     </div>
