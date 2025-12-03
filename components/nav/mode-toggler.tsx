cb1e8fb04a3ef80790b40d645b76c9ed074c9cc3
Nimira43 <n.rabone72@aol.com>
2025-12-03 22:16:14 +0000

    Import useTheme. Destructure setTheme

--- components/nav/mode-toggler.tsx
+++ components/nav/mode-toggler.tsx
@@ -1,1 +1,13 @@
+'use client'
+
+import { useTheme } from 'next-themes'
 import { GoSun, GoMoon } from 'react-icons/go'
+
+export default function ModeToggler() {
+  const { setTheme } = useTheme()
+
+  return (
+
+    <div>ModeToggler</div>
+  )
+}
