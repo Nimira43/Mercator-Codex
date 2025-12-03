f569b6b2e9b5da79f96a358e756259daf9aa1d05
Nimira43 <n.rabone72@aol.com>
2025-12-03 22:50:37 +0000

    ModeToggler - 3

--- components/nav/mode-toggler.tsx
+++ components/nav/mode-toggler.tsx
@@ -2,7 +2,7 @@
 
 import { useTheme } from 'next-themes'
 import { GoSun, GoMoon } from 'react-icons/go'
-import { DropdownMenu, DropdownMenuTrigger } from '../ui/dropdown-menu'
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from '../ui/dropdown-menu'
 import { Button } from '../ui/button'
 
 export default function ModeToggler() {
@@ -15,11 +15,18 @@ export default function ModeToggler() {
           variant='outline'
           size='icon'
         >
-          <GoSun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <GoMoon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <GoSun className='h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90' />
+          <GoMoon className='absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0' />
         </Button>
       </DropdownMenuTrigger>
+      <DropdownMenuContent align='end'>
+        <DropdownMenuItem onClick={() => setTheme('light')}>
+          Light
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('dark')}>
+          Dark
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
-    
   )
 }
