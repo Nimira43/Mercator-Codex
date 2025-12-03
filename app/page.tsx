2afe0103451da470c0988038cfa7513499c3529e
Nimira43 <n.rabone72@aol.com>
2025-12-03 21:46:20 +0000

    Defining layer components. Cleaning up HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -2,18 +2,18 @@ import { Button } from "@/components/ui/button";
 
 export default function HomePage() {
   return (
-    <>
-      <div>
-      <h1 className='text-3xl font-medium p-6 bg-main-dark text-main-light border-b-4 border-main logo'>
-        Mercator Codex
-      </h1>
+    <div className='flex '>
+      
     </div>
-    <div className='mt-10 p-6'>
-      <Button variant='mainBtn' className='m-2 transitioning'>Login</Button>
-      <Button variant='mainDarkBtn' className='m-2 transitioning'>Register</Button>
-      <Button variant='mainLightBtn' className='m-2 transitioning'>Submit</Button>
-    </div>
-    </>
-    
   )
 }
+
+
+{/* <h1 className='text-3xl font-medium p-6 bg-main-dark text-main-light border-b-4 border-main logo'>
+  Mercator Codex
+</h1> */}
+
+{/* <Button variant='mainBtn' className='m-2 transitioning'>Login</Button>
+<Button variant='mainDarkBtn' className='m-2 transitioning'>Register</Button>
+<Button variant='mainLightBtn' className='m-2 transitioning'>Submit</Button>
+<Button variant='outline' className='m-2 transitioning'>Cancel</Button> */}
