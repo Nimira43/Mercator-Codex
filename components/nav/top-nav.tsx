5e2907049bbb46e8ddd06ff995fee42e51b2ed83
Nimira43 <n.rabone72@aol.com>
2025-12-04 06:36:47 +0000

    TopNav 1

--- components/nav/top-nav.tsx
+++ components/nav/top-nav.tsx
@@ -1,8 +1,20 @@
-import { Menubar } from '../ui/menubar'
+import { Menubar, MenubarMenu } from '../ui/menubar'
 import ModeToggler from './mode-toggler'
 
 export default function TopNav() {
   return (
-    <div>TopNav</div>
+    <Menubar>
+      <div className='flex-none'>
+        <MenubarMenu>
+          Logo
+        </MenubarMenu>
+      </div>
+      <div className='centre-items flex-grow justify-end gap-1'>
+        <MenubarMenu>
+          
+        </MenubarMenu>
+      </div>
+    </Menubar>
+    
   )
 }
