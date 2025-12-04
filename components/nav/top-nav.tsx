ec0f394d27d48ba798443965c3dfd3f4a03e22cc
Nimira43 <n.rabone72@aol.com>
2025-12-04 06:51:14 +0000

    TopNav 2

--- components/nav/top-nav.tsx
+++ components/nav/top-nav.tsx
@@ -1,4 +1,4 @@
-import { Menubar, MenubarMenu } from '../ui/menubar'
+import { Menubar, MenubarContent, MenubarItem, MenubarMenu, MenubarSeparator, MenubarTrigger } from '../ui/menubar'
 import ModeToggler from './mode-toggler'
 
 export default function TopNav() {
@@ -11,7 +11,14 @@ export default function TopNav() {
       </div>
       <div className='centre-items flex-grow justify-end gap-1'>
         <MenubarMenu>
-          
+          <MenubarTrigger className='text-base font-medium'>
+            Dashboard
+          </MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>Menu Item 1</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Menu Item 2</MenubarItem>
+          </MenubarContent>
         </MenubarMenu>
       </div>
     </Menubar>
