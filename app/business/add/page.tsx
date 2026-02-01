7871b66f25c8ff7ee7a3fddd806f3d424baa31b5
Nimira43 <n.rabone72@aol.com>
2026-02-01 20:48:48 +0000

    SEtting up page layout for AddBusinessPAge,

--- app/business/add/page.tsx
+++ app/business/add/page.tsx
@@ -1,7 +1,8 @@
 export default function AddBusinessPage() {
   return (
-    <div className='centre h-screen'>
-        <h1>Add Business Page</h1>
+    <div className='flex flex-col lg:flex-row h-screen'>
+      <div className='flex flex-col lg:w-1/2 p-4 lg:order-last lg:flex lg:justify-center lg:items-center'>Preview</div>
+      <div className='flex flex-col lg:w-1/2 p-4 lg:order-first lg:flex lg:justify-center lg:items-center'>Form</div>
     </div>
   )
 }
